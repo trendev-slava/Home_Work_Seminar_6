1b179808b64e88627540a684a37ec46423c13074
Вячеслав Трендев <trendev97@gmail.com>
2023-04-18 23:39:45 +0500

    initial commit

---
+++ task_1/Program.cs
@@ -0,0 +1,34 @@
+﻿int number = GetNumberInput("сколько чисел вы хотите проверить?");
+int[] array = GetAndFillArray(number);
+Console.WriteLine($"-->{CheckPositiveNumbers(array)}");
+
+
+int GetNumberInput(string str = "")
+{
+    Console.WriteLine(str);
+    string input = Console.ReadLine();
+    return int.Parse(input);
+}
+
+int[] GetAndFillArray(int size)
+{
+    int[] array = new int[size];
+
+for (int i = 0; i < size; i++)
+{
+
+    array[i]=GetNumberInput($"Введите {i+1}-е число: ");
+}
+
+    return array;
+}
+
+int CheckPositiveNumbers(int[] array)
+{
+    int count = 0;
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i] > 0) count++;
+}
+    return count;
+}
---
+++ task_2/Program.cs
@@ -0,0 +1,22 @@
+﻿double b1 = GetNumberInput("b1 = ");
+double k1 = GetNumberInput("k1 = ");
+double b2 = GetNumberInput("b2 = ");
+double k2 = GetNumberInput("k2 = ");
+
+double[] result = IntersectionPoint(b1,k1,b2,k2);
+Console.WriteLine($"{result[0]}, {result[1]}");
+
+double[] IntersectionPoint(double b1, double k1, double b2, double k2)
+{
+    double[] res = new double[2];
+    res[0] = (b2 - b1) / (k1 - k2);
+    res[1] = k2 * res[0] + b2;
+    return res;
+}
+
+double GetNumberInput(string str = "")
+{
+    Console.Write(str);
+    string input = Console.ReadLine();
+    return double.Parse(input);
+}
